c6b83ae0c3ce14fe08f8825ddedaf72c852a1845
KoBold <kovacsboldizsar@gmail.com>
2020-04-01 21:07:49 +0300

    First GET

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
-import ujHaz from './UjHaz';
+import UjHaz from './UjHaz';
 import szures from './Szures';
 import home from './Home';
 
@@ -13,7 +13,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={home} />
-          <Route path="/ujHaz" component={ujHaz} />
+          <Route path="/ujHaz" component={UjHaz} />
           <Route path="/szures" component={szures} />
         </Switch>
       </div>
--- src/UjHaz.js
+++ src/UjHaz.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function ujHaz() {
+function UjHaz() {
+    const [hazUrl, setHazUrl] = useState('');
+    const updateHazUrl = e => {
+        setHazUrl(e.target.value);
+    };
+
+    const magic = () => {
+        console.log(hazUrl);
+        
+        
+        var xhr = new XMLHttpRequest()
+
+        // get a callback when the server responds
+        xhr.addEventListener('load', () => {
+        // update the state of the component with the result here
+            console.log(xhr.responseText)
+        })
+        // open the request with the verb and the url
+        xhr.open('GET', 'http://localhost:8080/kbk/')
+        // send the request
+        xhr.send()
+    };
+
     return (
-        <div>
-            <h3>Új Ház</h3>
-            <form>
-                <input type="text" name="name" />
-                <input type="submit" value="Vingardium Leviosaaaa" />
-            </form>
-        </div>
+        <form>
+            <input type="text" value={hazUrl} size="50" onChange={updateHazUrl} />
+            <button type="button" onClick={magic}>VingardiumLeviosaaa</button>
+        </form>
     );
 }
 
-export default ujHaz;
+export default UjHaz;
